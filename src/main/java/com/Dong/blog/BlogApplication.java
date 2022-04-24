7d9b2ba1f1e3f90c354569247ae7d966f51b1656
water-beetle <60806919+water-beetle@users.noreply.github.com>
2022-04-24 23:46:19 +0900

    env setting

---
+++ src/main/java/com/Dong/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.Dong.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
